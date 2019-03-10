37d532cd23b0343c4935f91118337b8b46114e07
Ashka <ashpuj@umich.edu>
2019-03-09 22:19:01 -0500

    updated rpn.py and test_rpn.py with correct code

--- test_rpn.py
+++ test_rpn.py
@@ -6,6 +6,16 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2, result)
+    def test_subtract(self):
+        result = rpn.calculate('4 2 -')
+        self.assertEqual(2, result)
+    def test_multiply(self)
+        result = rpn.calculate('3 4 *')
+        self.assertEqual(12, result)
+    def test_divide(self):
+        result = rpn.calculate('9 3 /')
+        self.assertEqual(3, result)
+
 
 #TestBasics tb;
 #tb.test_add() ==> TestBasics.test_add(tb)
